71a5f91dc60d78c32dd36223d560deb38a51a54f
Ojung-ii <ojh7839@gmail.com>
2023-07-05 12:58:17 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -54,7 +54,7 @@ const CONFIG = {
   utterances: {
     enable: true,
     config: {
-      repo: "morethanmin/morethan-log",
+      repo: "Ojung-ii/morethan-log",
       "issue-term": "og:title",
       label: "💬 Utterances",
     },
